7ff56f8b74dd73b40523f877c43c377527f42bcd
milobyte <amilesstanley@gmail.com>
2025-04-30 14:13:11 -0400

    Implemented components and updated dependencies.

---
+++ client/src/App.jsx
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+// import Body from './components/Body'
+import ContactInfo from './components/ContactInfo';
+import Bottom from './components/Bottom';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Header />
+      {/* <Body /> */}
+      <ContactInfo />
+      <Bottom />
+      <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ client/src/components/Bottom.jsx
@@ -0,0 +1,15 @@
+function Bottom() {
+    return (
+        <footer class="footer-section" id="footer-section">
+            <div class="container-fluid">
+                <div class="row text-center pt-3">
+                    <a class="col-md-6 p-3" href="https://www.linkedin.com/in/miles-stanley-406ab3191/"><img src="images/LI-Logo.png" /></a>
+                    <a class="col-md-6 p-3" href="https://github.com/milobyte"><img src="images/GitHub_Logo.png" /></a>
+                    <p>©2025 MILES STANLEY</p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Bottom;
